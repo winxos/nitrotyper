4a2dcf7d556edc323995d684eac8f9ded9e3ab5a
winxos <winxos@hotmail.com>
2017-03-24 01:19:56 +0800

    优化了结构 还有待提高

--- nitrotyper.py
+++ nitrotyper.py
@@ -80,9 +80,10 @@ def found_sub():
 
 
 def all_press():
-    s = []
-    s += [chr(k) for k in range(97, 123)]
+    s = ['f', 'c', 'y', 'r', 'o', 'k']
+    s += ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
     pg.press(s)
+    print("[debug] all pressed.")
 
 
 def get_roi_bounding_rect(img, color_min, color_max):
@@ -92,57 +93,71 @@ def get_roi_bounding_rect(img, color_min, color_max):
     return cv2.boundingRect(roi_mask)  # x,y,w,h
 
 
-def main_loop():
+def get_bound():
     bx, by, bw, bh = found_sub()
     r = (bx - 5, by, bx + bw * 42, by + bh)
+    return r, bw, bh
+
+
+def main_loop():
+    r, bw, bh = get_bound()
     enter_counter = 0
+    last_x = 0
+    miss = 0
     while True:
+        if miss == 150:
+            print("[debug] enter.")
+            pg.press("enter")
+            miss = 0
+            time.sleep(2)
+            r, _, _ = get_bound()
+        elif miss == 500:
+            pg.press("f5")
+            time.sleep(2)
         im = pg.screenshot()
         sub = im.crop(r)
         cvs = np.array(sub)
         cvs = cv2.cvtColor(cvs, cv2.COLOR_BGR2RGB)
         # cvs = cv2.cvtColor(cvs, cv2.COLOR_BGR2GRAY)
-        roi_color = np.array([160, 234, 172]) #BGR of normal green box
+        roi_color = np.array([160, 234, 172])  # BGR of normal green box
         x, y, w, h = get_roi_bounding_rect(cvs, roi_color, roi_color)
-
         if w * h != bw * bh:  # err
-            roi_color_error = np.array([160, 170, 234]) #error red box
+            roi_color_error = np.array([160, 170, 234])  # error red box
             x, y, w, h = get_roi_bounding_rect(cvs, roi_color_error, roi_color_error)
-            if w * h != bw * bh:
-                enter_counter += 1
-                if enter_counter > 50:
-                    pg.press("enter")
-                    print(enter_counter)
-                    print("w,h: %d %d" % (w, h))
-                    if enter_counter > 500:
-                        # print("[debug] not found.")
-                        print((x, y, w, h))
-                        pg.press("f5")
-                        time.sleep(2)
-                        pg.press("enter")
-                        enter_counter = 0
-                    time.sleep(0.1)
-                    continue
-        if w * h == bw * bh:  # w*h
-            im_char = cvs[y:y + h, x:x + w, :]
-            im_char = cv2.cvtColor(im_char, cv2.COLOR_RGB2GRAY)
-            t, im_char = cv2.threshold(im_char, 0, 255, cv2.THRESH_OTSU)
-            # im_char = cv2.resize(im_char, (8, 9), interpolation=cv2.INTER_AREA)
-            # cv2.imshow("im", im_char)
-            ch = image_decode(im_char)
-            print(ch[:3])
-            if ch[0][0] < 20:  # auto press
-                pg.press(ch[0][1])
-                enter_counter = 0
-            else:
+            if w * h != bw * bh:  # nothing found.
+                print("[debug] miss %d." % miss)
+                miss += 1
+                continue
+        if last_x == x:  #
+            enter_counter += 1
+            print("[debug] retry times %d" % enter_counter)
+            if enter_counter == 3:
                 all_press()
                 time.sleep(1)
-            img_h = img_dhash(im_char)
-            if img_h not in imgs:
-                imgs.append(img_h)
-                cv2.imwrite("./data/%s.png" % str(img_h), im_char)
-                # todo 自动录入实现
-                # cv2.imshow("raw", cvs)
+            elif enter_counter == 10:
+                pg.press("enter")
+                print("[debug] press enter")
+        else:
+            enter_counter = 0
+        last_x = x
+        im_char = cvs[y:y + h, x:x + w, :]
+        im_char = cv2.cvtColor(im_char, cv2.COLOR_RGB2GRAY)
+        t, im_char = cv2.threshold(im_char, 0, 255, cv2.THRESH_OTSU)
+        # im_char = cv2.resize(im_char, (8, 9), interpolation=cv2.INTER_AREA)
+        # cv2.imshow("im", im_char)
+        ch = image_decode(im_char)
+        print("[debug] recognize %s" % str(ch[:3]))
+        if ch[0][0] < 20:  # auto press
+            # print("[debug] press '%s'" % ch[0][1])
+            pg.press(ch[0][1])
+        else:
+            all_press()
+        # img_h = img_dhash(im_char)
+        # if img_h not in imgs:
+        #     imgs.append(img_h)
+        #     cv2.imwrite("./data/%s.png" % str(img_h), im_char)
+
+        cv2.imshow("raw", cvs)
         time.sleep(0.05)
 
         key = cv2.waitKey(1)
