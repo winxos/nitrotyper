9a4acd5058fea4f243e8b290a0541d6b7af7d879
winxos <winxos@hotmail.com>
2017-03-25 15:52:02 +0800

    结构优化

--- nitrotyper.py
+++ nitrotyper.py
@@ -10,44 +10,29 @@ import numpy as np
 import time
 import json
 
+TYPE_SPEED_DELAY = 0.05
+IS_SAMPLER = False
 NORMAL_CHAR_BOX_COLOR = np.array([160, 234, 172])
 ERROR_CHAR_BOX_COLOR = np.array([160, 170, 234])
 
 
-def img_hash(src):
-    return cv2.countNonZero(src)
-
-
-#
-# def img_dhash(src):
-#     h, w = src.shape[:2]
-#     hash = ""
-#     for r in range(h - 1):
-#         for c in range(w):
-#             if src[r][c] != src[r + 1][c]:
-#                 hash += "1"
-#             else:
-#                 hash += "0"
-#     return '%0*X' % ((len(hash) + 3) // 4, int(hash, 2))
 def img_dhash(src):
     h, w = src.shape[:2]
-    hash = ""
+    hash_str = ""
     for r in range(h - 1):
         for c in range(w):
-            hash += "%d" % (src[r][c] == 0)
-    return '%0*X' % ((len(hash) + 3) // 4, int(hash, 2))
+            hash_str += "%d" % (src[r][c] == 0)
+    return '%0*X' % ((len(hash_str) + 3) // 4, int(hash_str, 2))
 
 
 def hash_diff(h1, h2):
     if len(h1) != len(h2):
-        print("%d %d" % (len(h1), len(h2)))
-        print("h1 %s" % h1)
-        print("h2 %s" % h2)
+        print("[error] hash length not equal, %s ,%s" % (h1, h2))
         return None
     diff = 0
     h1 = bin(int(h1, 16))[2:]
     h2 = bin(int(h2, 16))[2:]
-    h1 = '0' * (300 - len(h1)) + h1
+    h1 = '0' * (300 - len(h1)) + h1  # make same length
     h2 = '0' * (300 - len(h2)) + h2
     # print(h1)
     for i, _ in enumerate(h1):
@@ -56,7 +41,7 @@ def hash_diff(h1, h2):
     return diff
 
 
-def image_decode(img):
+def image_recognize(img):
     mh = img_dhash(img)
     res = []
     for i in CONFIG.keys():
@@ -65,10 +50,14 @@ def image_decode(img):
     return sorted(res)
 
 
-imgs = []
+def all_press():
+    s = ['f', 'c', 'y', 'r', 'o', 'k']
+    s += ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
+    pg.press(s)
+    print("[debug] all pressed.")
 
 
-def found_sub():
+def found_bound():
     timer = 0
     while True:
         im = pg.screenshot()
@@ -92,13 +81,6 @@ def found_sub():
     return x, y, w, h
 
 
-def all_press():
-    s = ['f', 'c', 'y', 'r', 'o', 'k']
-    s += ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
-    pg.press(s)
-    print("[debug] all pressed.")
-
-
 def get_roi_bounding_rect(img, color_min, color_max):
     roi_mask = cv2.inRange(img.copy(), color_min, color_max)
     roi_mask = cv2.erode(roi_mask, None, iterations=1)
@@ -107,9 +89,19 @@ def get_roi_bounding_rect(img, color_min, color_max):
 
 
 def get_bound():
-    bx, by, bw, bh = found_sub()
-    r = (bx - 5, by, bx + bw * 42, by + bh * 2)
+    bx, by, bw, bh = found_bound()
+    r = (bx - 5, by, bx + bw * 42, by + bh)
     return r, bw, bh
+
+
+imgs = []
+
+
+def sampler(im_char):
+    img_h = img_dhash(im_char)
+    if img_h not in imgs:
+        imgs.append(img_h)
+        cv2.imwrite("./data/%s.png" % str(img_h), im_char)
 
 
 def main_loop():
@@ -118,7 +110,7 @@ def main_loop():
     last_x = 0
     miss = 0
     while True:
-        if 300 > miss > 200:
+        if 300 > miss > 200:  # nothing found state
             print("[debug] enter.")
             pg.press("enter")
             time.sleep(1)
@@ -140,7 +132,7 @@ def main_loop():
                 continue
         else:
             miss = 0
-        if last_x == x:  #
+        if last_x == x:  # type miss match error state
             enter_counter += 1
             print("[debug] retry times %d" % enter_counter)
             if enter_counter == 6:
@@ -155,20 +147,14 @@ def main_loop():
         im_char = cvs[y:y + h, x:x + w, :]
         im_char = cv2.cvtColor(im_char, cv2.COLOR_RGB2GRAY)
         t, im_char = cv2.threshold(im_char, 0, 255, cv2.THRESH_OTSU)
-        # im_char = cv2.resize(im_char, (8, 9), interpolation=cv2.INTER_AREA)
-        # cv2.imshow("im", im_char)
-        ch = image_decode(im_char)
+        ch = image_recognize(im_char)
         print("[debug] recognize %s" % str(ch[:3]))
         if ch[0][0] < 20:  # auto press
-            # print("[debug] press '%s'" % ch[0][1])
             pg.press(ch[0][1])
-        # img_h = img_dhash(im_char)
-        # if img_h not in imgs:
-        #     imgs.append(img_h)
-        #     cv2.imwrite("./data/%s.png" % str(img_h), im_char)
-
-        cv2.imshow("raw", cvs)
-        time.sleep(0.05)
+        if IS_SAMPLER:
+            sampler(im_char)
+        # cv2.imshow("raw", cvs)
+        time.sleep(TYPE_SPEED_DELAY)
 
         key = cv2.waitKey(1)
         if key == 27:
@@ -185,16 +171,11 @@ except IOError as e:
 
 
 def test():
-    m = cv2.imread(
-        "./data/0000000000000000000000000000F81640101800F01702000002100721EE000000000000000.png",
-        cv2.IMREAD_GRAYSCALE)
-    t, m = cv2.threshold(m, 0, 255, cv2.THRESH_OTSU)
-    print(image_decode(m)[:3])
-    # time.sleep(1)
     # while True:
     #     s = [' ', '.', ',', '\'','?','!']
     #     s += [chr(k) for k in range(65, 123)]
     #     pg.press(s)
+    pass
 
 
 if __name__ == "__main__":
